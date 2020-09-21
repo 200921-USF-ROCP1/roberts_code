c4adcb6c975a8fd9cbed3687d94c6aa1454b0235
leer <ladesigns90@gmail.com>
2020-09-21 16:12:00 -0400

    Hello World project. Packages and control flow basics

---
+++ week1/HelloWorld/src/com/revature/app/HelloWorldTwo.java
@@ -0,0 +1,11 @@
+package com.revature.app;
+
+import com.revature.lee.*;
+
+public class HelloWorldTwo {
+	public static void main(String[] args) {
+		Lee l;
+		System.out.println();
+	}
+	
+}
---
+++ week1/HelloWorld/src/com/revature/lee/Lee.java
@@ -0,0 +1,9 @@
+package com.revature.lee;
+
+public class Lee {
+	public static void main(String[] args) {
+		
+		System.out.println();
+	}
+
+}
